d7282e10f695df162211da7f5a5b4344c8b5370a
taro-aoki <philoMP@gmail.com>
2016-10-09 12:26:29 +0900

    first commit

---
+++ ConsoleHello.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Console_Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello");
+
+            Console.ReadKey();
+        }
+    }
+}
